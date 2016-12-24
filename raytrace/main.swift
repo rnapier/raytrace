333b2ec64b288b329a18428fa40960198a6ee629
Rob Napier <robnapier@gmail.com>
2016-12-23 22:09:14 -0500

    First commit. Hello world

--- raytrace/main.swift
+++ raytrace/main.swift
@@ -6,7 +6,17 @@
 //
 //
 
-import Foundation
-
-print("Hello, World!")
-
+let nx = 200
+let ny = 100
+print("P3\n\(nx) \(ny)\n255")
+for j in (0..<ny).reversed() {
+    for i in 0..<nx {
+        let r = Float(i) / Float(nx)
+        let g = Float(j) / Float(ny)
+        let b = 0.2
+        let ir = Int(255.99 * r)
+        let ig = Int(255.99 * g)
+        let ib = Int(255.99 * b)
+        print("\(ir) \(ig) \(ib)")
+    }
+}
